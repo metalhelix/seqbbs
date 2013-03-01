2f636e66f5872ff22d0fa6ae1a54ca4f30a26fdd
Jim V <jfv@stowers.org>
2013-03-01 15:17:08 -0600

    use actual test data

--- inst/tests/test-seqbbs.r
+++ inst/tests/test-seqbbs.r
@@ -1,9 +1,11 @@
 context("Basic Validitiy Checks")
 
 test_that("function seqbbs", {
-  window <- 2
+  window <- 20
   threshold <- 0.55
   ratios <- c(0.2, 0.4, 0.6, 0.4)
+  test_filename <- system.file("extdata", "test.txt", package="seqbbs")
+  ratios <- read.table(test_filename, header = FALSE)
   results <- seqbbs(ratios, window = window, threshold = threshold)
   
 })
